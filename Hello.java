c30814025e7476f7e7d664d9ebc30280a6350a20
Joseph-ADAMAH <adamahkouevijoseph@gmail.com>
2025-09-10 15:16:39 +0000

    Ajout à Hello word mon nom et prénom

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
-	public static void main (String [] ags){
-		System.out.println("Hello World");
+	public static void main (String [] ags)
+    {
+
+        System.out.println("Hello World, I'm Joseph ADAMAH ");
+
 	}
 }
